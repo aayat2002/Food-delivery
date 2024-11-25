789c26b5fab45645a06b41212177d6ce62222900
Sadqua parween <f.naaz.2002a@gmail.com>
2024-11-25 21:08:31 +0530

    navbar

--- food-del/public/src/main.jsx
+++ food-del/public/src/main.jsx
@@ -4,11 +4,13 @@ import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import StoreContextProvider from "./components/context/StoreContext.jsx";
+import Navbar from "./components/Navbar/Navbar.jsx";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <StoreContextProvider>
       <App />
+      <Navbar />
     </StoreContextProvider>
   </BrowserRouter>
 );
